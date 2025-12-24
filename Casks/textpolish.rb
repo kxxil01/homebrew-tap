db949661525bbbc4b0346b2d6c030060debdcf52
Ilham <ilham@Ilhams-MacBook-Pro.local>
2025-12-24 21:47:51 +0700

    Add TextPolish cask formula

---
+++ Casks/textpolish.rb
@@ -0,0 +1,18 @@
+cask "textpolish" do
+  version "1.0.0"
+  sha256 "REPLACE_WITH_SHA256_AFTER_RELEASE"
+
+  url "https://github.com/kxxil01/TextPolish/releases/download/v#{version}/TextPolish.app.zip"
+  name "TextPolish"
+  desc "Grammar correction anywhere on macOS"
+  homepage "https://github.com/kxxil01/TextPolish"
+
+  depends_on macos: ">= :sonoma"
+
+  app "TextPolish.app"
+
+  zap trash: [
+    "~/Library/Application Support/TextPolish",
+    "~/Library/Preferences/com.kxxil01.TextPolish.plist",
+  ]
+end
