9422d0d643efe47bb921be70c4fda5c6dffa649d
Kurniadi Ilham <85265656+kxxil01@users.noreply.github.com>
2026-03-11 17:45:14 +0000

    kubectui 1.0.0

--- Formula/kubectui.rb
+++ Formula/kubectui.rb
@@ -7,20 +7,20 @@ class Kubectui < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kxxil01/Kubectui/releases/download/v#{version}/kubectui-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "855820dd15d4674a7f7482d2b14e7517c7cc95d9a1a0dd6733e8a5793c8dbb0f"
     else
       url "https://github.com/kxxil01/Kubectui/releases/download/v#{version}/kubectui-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "ebbb1ce2005b2c9ebd811daabde241729fff72df008d368428e0527d62326b2e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/kxxil01/Kubectui/releases/download/v#{version}/kubectui-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "0a18caf3b4c5058632f3292735dae4728e64db048828813570b99deb6bd99ac6"
     else
       url "https://github.com/kxxil01/Kubectui/releases/download/v#{version}/kubectui-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "4ff69ee87247a627dfc4fba0a7b2528bbd6f14149792616c6bb21fde105c3e90"
     end
   end
 
