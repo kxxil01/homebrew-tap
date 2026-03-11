5e5064d80266cd20b476e9d0429851ef25d05d15
Kurniadi Ilham <85265656+kxxil01@users.noreply.github.com>
2026-03-11 23:50:24 +0700

    feat: add kubectui formula

---
+++ Formula/kubectui.rb
@@ -0,0 +1,34 @@
+class Kubectui < Formula
+  desc "Keyboard-first terminal UI for Kubernetes operations"
+  homepage "https://github.com/kxxil01/Kubectui"
+  version "1.0.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/kxxil01/Kubectui/releases/download/v#{version}/kubectui-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/kxxil01/Kubectui/releases/download/v#{version}/kubectui-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/kxxil01/Kubectui/releases/download/v#{version}/kubectui-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/kxxil01/Kubectui/releases/download/v#{version}/kubectui-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install "kubectui"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/kubectui --version")
+  end
+end
