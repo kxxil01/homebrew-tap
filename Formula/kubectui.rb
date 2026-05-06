b25544df5a7015b8c503010cc13c2dee558a8358
Kurniadi Ilham <85265656+kxxil01@users.noreply.github.com>
2026-05-06 17:23:46 +0700

    kubectui 1.1.19 literal URLs

--- Formula/kubectui.rb
+++ Formula/kubectui.rb
@@ -5,20 +5,20 @@ class Kubectui < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kxxil01/Kubectui/releases/download/v#{version}/kubectui-v#{version}-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/kxxil01/Kubectui/releases/download/v1.1.19/kubectui-v1.1.19-aarch64-apple-darwin.tar.gz"
       sha256 "6dd54810b38d1975c2855a74b6bfa32861f43e86cc942eae02cb6ba71276d1d3"
     else
-      url "https://github.com/kxxil01/Kubectui/releases/download/v#{version}/kubectui-v#{version}-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/kxxil01/Kubectui/releases/download/v1.1.19/kubectui-v1.1.19-x86_64-apple-darwin.tar.gz"
       sha256 "0d3c12e65604c8d8210f5b27db20ff755501c9d97579cc7bc85e3343ee14c33f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kxxil01/Kubectui/releases/download/v#{version}/kubectui-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/kxxil01/Kubectui/releases/download/v1.1.19/kubectui-v1.1.19-aarch64-unknown-linux-gnu.tar.gz"
       sha256 "c23152f32a1f89882aa2d979d28a780690132350676c5a831f6955e5c292eca4"
     else
-      url "https://github.com/kxxil01/Kubectui/releases/download/v#{version}/kubectui-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/kxxil01/Kubectui/releases/download/v1.1.19/kubectui-v1.1.19-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "3d1bc5e310290ca15944ff179a8d462c925ce1d7bec774e06803e8e61bf8c16a"
     end
   end
